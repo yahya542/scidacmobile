6659c7825041b2b35a21689ddf2b09a23fd893b6
yahya <muhyahyaas@gmail.com>
2025-03-24 22:46:59 +0700

    all fitur is ready functioned

---
+++ app/fitur/activity/db_activity.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const db_activity = () => {
+  return (
+    <View>
+      <Text>db_activity</Text>
+    </View>
+  )
+}
+
+export default db_activity
+
+const styles = StyleSheet.create({})
