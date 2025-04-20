22ea1fdd1bf8d1642150b98ee96efdb5f14feb39
yahya <muhyahyaas@gmail.com>
2025-04-20 11:17:37 +0700

    edit layout on profilescreen

---
+++ component/edit.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const edit = () => {
+  return (
+    <View>
+      <Text>edit</Text>
+    </View>
+  )
+}
+
+export default edit
+
+const styles = StyleSheet.create({
+    
+})
